0fa05f43129ce7b325a91d74fae910300fee4ff0
Luiz Gustavo <62274995+luizgcl@users.noreply.github.com>
2025-06-03 01:02:22 -0300

    landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 import { TigrinComponent } from './pages/tigrin/tigrin.component';
 
 export const routes: Routes = [
     {
         path: '',
+        component: LandingComponent,
+    },
+    {
+        path: 'play',
         component: TigrinComponent
     }
 ];
